40499fbb743e2f31b577e4aa22b7743dcf725ae7
Ahmad Faraz Ansari <Frzansari20@gmail.com>
2024-08-16 01:39:05 +0530

    complete signup api route

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,56 @@
+import bcryptjs from "bcryptjs";
+import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
+
 export const signup = async (req, res) => {
-    res.json({ message: "Hello from the signup controller" });
+    try {
+        const { username, email, fullname, password } = req.body;
+
+        const existingUser = await User.findOne({ username });
+        if (existingUser) {
+            return res.status(400).json({ message: "User already exists" });
+        }
+
+        const existingEmail = await User.findOne({ email });
+        if (existingEmail) {
+            return res.status(400).json({ message: "Email already exists" });
+        }
+
+        if (password.length < 6) {
+            return res.status(400).json({
+                message: "Password must be at least 6 characters long",
+            });
+        }
+
+        const passwordHash = await bcryptjs.hash(password, 10);
+
+        const user = await User.create({
+            username,
+            email,
+            fullname,
+            password: passwordHash,
+        });
+
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+            expiresIn: "7d",
+        });
+
+        res.cookie("jwt", token, {
+            maxAge: new Date(Date.now() + 60 * 60 * 24 * 7 * 1000), // 7 days
+            httpOnly: true,
+            sameSite: "strict",
+            secure: process.env.NODE_ENV === "production",
+        });
+
+        const { password: userPassword, ...userWithoutPassword } = user._doc;
+
+        res.status(201).json(userWithoutPassword);
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).json({
+            message: "Internal server error occurred during signup",
+        });
+    }
 };
 
 export const login = async (req, res) => {

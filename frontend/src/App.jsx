fb2125dbab45a364ea687beff7109a592fced219
Ahmad Faraz Ansari <Frzansari20@gmail.com>
2024-08-18 18:19:40 +0530

    install tailwind css and setup daisyUI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,10 @@
-function App() {
-    return <div>hello</div>;
+export default function App() {
+    return (
+        <div className="flex gap-6 items-center justify-center mt-72">
+            <button className="btn btn-outline btn-info">Info</button>
+            <button className="btn btn-outline btn-success">Success</button>
+            <button className="btn btn-outline btn-warning">Warning</button>
+            <button className="btn btn-outline btn-error">Error</button>
+        </div>
+    );
 }
-
-export default App;

5b84ad636220411c7b3505cb13aae526261b2158
Ahmad Faraz Ansari <Frzansari20@gmail.com>
2024-08-21 01:10:40 +0530

    complete UI design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,26 @@
+import { Routes, Route } from "react-router-dom";
+
+import SignUp from "./pages/auth/signup/SignUp";
+import Login from "./pages/auth/login/Login";
+import Home from "./pages/home/Home";
+import NotificationPage from "./pages/notification/NotificationPage";
+import ProfilePage from "./pages/profile/ProfilePage";
+
+import SideBar from "./components/common/SideBar";
+import RightPanel from "./components/common/RightPanel";
+
 export default function App() {
     return (
-        <div className="flex gap-6 items-center justify-center mt-72">
-            <button className="btn btn-outline btn-info">Info</button>
-            <button className="btn btn-outline btn-success">Success</button>
-            <button className="btn btn-outline btn-warning">Warning</button>
-            <button className="btn btn-outline btn-error">Error</button>
+        <div className="flex max-w-6xl mx-auto">
+            <SideBar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/notifications" element={<NotificationPage />} />
+                <Route path="/signup" element={<SignUp />} />
+                <Route path="/profile/:username" element={<ProfilePage />} />
+            </Routes>
+            <RightPanel />
         </div>
     );
 }

3f8daa27b40570988dfdc16f0b5d21e7d103b9b5
Ahmad Faraz Ansari <Frzansari20@gmail.com>
2024-08-22 02:06:18 +0530

    implement post fetching functionality for Home page

--- frontend/src/components/common/Posts.jsx
+++ frontend/src/components/common/Posts.jsx
@@ -1,27 +1,64 @@
 import Post from "./Post";
 import PostSkeleton from "../skeletons/PostSkeleton";
-import { POSTS } from "../../utils/db/dummy";
+import { useQuery } from "@tanstack/react-query";
+import { useEffect } from "react";
 
-export default function Posts() {
-    const isLoading = false;
+// FeedType is a prop that will be passed to Posts component to determine which posts to show
+// FeedType can either be "for you" or "following"
+export default function Posts({ feedType }) {
+    const getPostsEndpoint = () => {
+        if (feedType === "forYou") {
+            return "/api/post/posts";
+        } else {
+            return "/api/post/following-feed";
+        }
+    };
+
+    const POST_ENDPOINT = getPostsEndpoint();
+
+    const {
+        data: posts,
+        isLoading,
+        refetch,
+        isRefetching,
+    } = useQuery({
+        queryKey: ["posts"],
+        queryFn: async () => {
+            try {
+                const response = await fetch(POST_ENDPOINT);
+                const data = await response.json();
+                if (!response.ok) {
+                    throw new Error(data.message);
+                }
+
+                return data;
+            } catch (error) {
+                throw new Error(error);
+            }
+        },
+    });
+
+    useEffect(() => {
+        refetch();
+    }, [feedType, refetch]);
 
     return (
         <>
-            {isLoading && (
+            {(isLoading || isRefetching) && (
                 <div className="flex flex-col justify-center">
                     <PostSkeleton />
                     <PostSkeleton />
                     <PostSkeleton />
                 </div>
             )}
-            {!isLoading && POSTS?.length === 0 && (
+            {!isLoading && !isRefetching && posts?.length === 0 && (
                 <p className="text-center my-4">
                     No posts in this tab. Switch 👻
                 </p>
             )}
-            {!isLoading && POSTS && (
+            {!isLoading && !isRefetching && posts && (
                 <div>
-                    {POSTS.map((post) => (
+                    {posts.map((post) => (
                         <Post key={post._id} post={post} />
                     ))}
                 </div>
